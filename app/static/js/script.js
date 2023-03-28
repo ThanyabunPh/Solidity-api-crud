8355efbb83453252b16251bf09c717c857638d2a
thanlnwza <thanyabun.ph@gmail.com>
2023-03-28 21:51:40 +0700

    write_html_script

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -43,18 +43,160 @@ AddData = () => {
     }
 }
 
-editData = () => {
-    Swal.fire({
-        title: 'Edit Data',
-        html: '<input id="name" class="swal2-input" placeholder="Name">' +
-            '<input id="surname" class="swal2-input" placeholder="Surname">' +
-            '<input id="GPAX" class="swal2-input" placeholder="GPAX">',
-        focusConfirm: false,
-    })
+editData = (ID) => {
+    fetch('/get_by_id/' + ID, {
+        method: 'GET',
+    }).then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data) {
+                Swal.fire({
+                    title: 'Edit Data',
+                    html:
+                        '<div class="form-group">' +
+                        '<label for="ID">ID</label>' +
+                        '<input disabled id="ID" type="text" class="form-control" value="' + data.data[0] + '">' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                        '<label for="name">Address</label>' +
+                        '<input id="name" type="text" class="form-control" value="' + data.data[1] + '">' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                        '<label for="surname">Surname</label>' +
+                        '<input id="surname" type="text" class="form-control" value="' + data.data[2] + '">' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                        '<label for="GPAX">GPAX</label>' +
+                        '<input id="GPAX" type="text" class="form-control" value="' + data.data[3] + '">' +
+                        '</div>',
+                    focusConfirm: false,
+                    showCancelButton: true,
+                    confirmButtonText: 'Confirm',
+                })
+                    .then((result) => {
+                        if (result.isConfirmed) {
+                            const ID = Swal.getPopup().querySelector('#ID').value;
+                            const name = Swal.getPopup().querySelector('#name').value;
+                            const surname = Swal.getPopup().querySelector('#surname').value;
+                            const GPAX = Swal.getPopup().querySelector('#GPAX').value;
+
+                            if (name === '' || surname === '' || GPAX === '') {
+                                toast().fire({
+                                    icon: 'error',
+                                    title: 'Please fill all fields',
+                                })
+                            } else {
+                                fetch('/update', {
+                                    method: 'PUT',
+                                    headers: {
+                                        'Content-Type': 'application/json'
+                                    },
+                                    body: JSON.stringify({
+                                        "id": ID,
+                                        "name": name,
+                                        "surname": surname,
+                                        "GPAX": GPAX
+                                    })
+                                }).then(response => response.json())
+                                    .then(data => {
+                                        console.log(data);
+                                        if (data.result === 'success') {
+                                            toast().fire({
+                                                icon: 'success',
+                                                title: 'Successfully updated data',
+                                            }).then(() => {
+                                                window.location.reload();
+                                            })
+                                        }
+                                    })
+                                    .catch(error => {
+                                        toast().fire({
+                                            icon: 'error',
+                                            title: 'Something went wrong',
+                                        })
+                                    })
+                            }
+                        }
+                    });
+            }
+        });
+
 }
 
-deleteData = () => {
-    alert("Delete Data");
+deleteData = (ID) => {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch('/delete/', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "id": ID
+                })
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.result === 'success') {
+                        toast().fire({
+                            icon: 'success',
+                            title: 'Successfully deleted data',
+                        }).then(() => {
+                            window.location.reload();
+                        })
+                    }
+                })
+                .catch(error => {
+                    toast().fire({
+                        icon: 'error',
+                        title: 'Something went wrong',
+                    })
+                })
+        }
+    })
+
+}
+
+searchData = () => {
+    const name = document.getElementById("name").value;
+    const surname = document.getElementById("surname").value;
+    const GPAX = document.getElementById("GPAX").value;
+
+    if (name === '' && surname === '' && GPAX === '') {
+        toast().fire({
+            icon: 'error',
+            title: 'Please fill at least one field',
+        })
+    } else {
+        const List = [];
+        const tbody = document.getElementById('myTable');
+        const rows = tbody.querySelectorAll('tr');
+        for (let i = 0; i < rows.length; i++) {
+            const cells = rows[i].querySelectorAll('td');
+            if (name === cells[1].innerText || surname === cells[2].innerText || GPAX === cells[3].innerText) {
+                List.push(rows[i]);
+            }
+        }
+        if (List.length === 0) {
+            toast().fire({
+                icon: 'error',
+                title: 'No data found',
+            })
+        } else {
+            tbody.innerHTML = '';
+            List.forEach((item) => {
+                tbody.appendChild(item);
+            });
+        }
+    }
+
 }
 
 toast = () => {
